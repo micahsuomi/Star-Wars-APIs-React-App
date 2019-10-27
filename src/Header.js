f1810301299c7538a0575240d3ed2ba668789aff
Michele Zucca <michele.zucca81@gmail.com>
2019-10-27 21:11:31 +0200

    Sixth Commit

--- src/Header.js
+++ src/Header.js
@@ -19,7 +19,11 @@ class Header extends Component {
                         <div className="header-right-wrapper">
                         <h1 className="main-heading"> Star Wars</h1>
                         <h2 className="sub-heading">A Page Built With Star Wars APIs...</h2>
-                        <h2 className="sub-heading">and React <span className="react-logo-placeholder"><img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" className="react-logo" alt="react logo" /></span></h2>
+                        <div className="sub-heading-container">
+                        <h2 className="sub-heading">and React</h2>
+                        <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" className="react-logo" alt="react logo" />
+
+                        </div>
                         <div className='icon-scroll'></div>
 
                         </div>
