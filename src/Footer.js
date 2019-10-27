60977ecfecdfe490d719979092b86628190b08d4
Michele Zucca <michele.zucca81@gmail.com>
2019-10-27 19:09:26 +0200

    Second commit

--- src/Footer.js
+++ src/Footer.js
@@ -6,15 +6,18 @@ const Footer = (props) => {
     props = {
         headingSmall: "Star Wars APIs",
         footerText1: "View on Github",
-        footerText2: "Deployed on Netlify"
+        footerText2: "Deployed on Netlify",
+        footerText3: "View my Portfolio"
         
     }
     return(
         <div className="footer">
                 <ul className="footer-wrapper">
                 <li className="footer-text"><a href="https://swapi.co/" target="blank">{props.headingSmall}</a></li>
-                <li className="footer-text">{props.footerText1}</li>
-                <li className="footer-text">{props.footerText2}</li>
+                <li className="footer-text"><a href="https://github.com/micahsuomi/Star-Wars-APIs-React-App" target="blank">{props.footerText1}</a></li>
+                <li className="footer-text"><a href="https://www.netlify.com/">{props.footerText2}</a></li>
+                <li className="footer-text"><a href="https://www.michelezuccawebdev.com/">{props.footerText3}</a></li>
+
                 </ul>
         </div>
     )
